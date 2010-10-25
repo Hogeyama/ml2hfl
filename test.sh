a3618663288d10b4c080cbd1089e2aa88eaca18a
uhiro <uhiro@382e5e6e-cd79-4ffa-aa24-b240b7b59e18>
2010-10-25 14:14:27 +0000

    Test commit
    git-svn-id: file:///home/ryosuke/repos_test/mochi@2 382e5e6e-cd79-4ffa-aa24-b240b7b59e18

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TEST="intro1.ml intro2.ml intro3.ml sum.ml mult.ml max.ml mc91.ml repeat.ml lock.ml exception.ml neg1.ml neg2.ml neg3.ml neg4.ml fxx.ml fhnhn1.ml fhnhn2.ml intro1-e.ml intro2-e.ml intro3-e.ml sum-e.ml mult-e.ml mc91-e.ml lock-e.ml repeat-e.ml max-e.ml"
-TEST="intro1.ml intro2.ml intro3.ml sum.ml mult.ml max.ml mc91.ml repeat.ml fhnhn2.ml lock.ml neg4.ml sum-e.ml mult-e.ml max-e.ml mc91-e.ml repeat-e.ml lock-e.ml repeat2.ml"
+TEST="intro1.ml intro2.ml intro3.ml sum.ml mult.ml max.ml mc91.ml repeat.ml repeat2.ml fhnhn2.ml lock.ml neg4.ml sum-e.ml mult-e.ml max-e.ml mc91-e.ml repeat-e.ml lock-e.ml"
 
 for i in $TEST
 do
