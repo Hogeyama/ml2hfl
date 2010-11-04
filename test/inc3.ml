d157bb9176741621432cb3b29b83fd39f98fc945
uhiro <uhiro@382e5e6e-cd79-4ffa-aa24-b240b7b59e18>
2010-11-04 02:47:20 +0000

    Modify inc3.ml
    git-svn-id: file:///home/ryosuke/repos_test/mochi@20 382e5e6e-cd79-4ffa-aa24-b240b7b59e18

--- test/inc3.ml
+++ test/inc3.ml
@@ -1,12 +1,12 @@
 let make_array n i = assert(0<=i && i<n); 0 in
-let update des i x = let _ = des i in () in
+let update i n des x = let _ = des i in () in
 let rec inc3 m src i =
-  if i>=m
-  then ()
-  else
-    begin
-      update src i ((src i)+1);
-      inc3 m src (i+1)
-    end
+ if i>=m
+ then ()
+ else
+   begin
+     update i m src ((src i)+1);
+     inc3 m src (i+1)
+   end
 in
-  if n>0 then inc3 n (make_array n) 0 else ()
+ if n>0 then inc3 n (make_array n) i else ()
