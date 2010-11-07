29ae23b530f4b0913a7a0bc1d15af6acd39a2b84
uhiro <uhiro@382e5e6e-cd79-4ffa-aa24-b240b7b59e18>
2010-11-07 16:28:58 +0000

    Fix bug in lift2
    git-svn-id: file:///home/ryosuke/repos_test/mochi@22 382e5e6e-cd79-4ffa-aa24-b240b7b59e18

--- syntax.ml
+++ syntax.ml
@@ -592,7 +592,7 @@ let rec lift2_aux xs = function
   | Letrec(f,ys,t1,t2) ->
       let fv = xs in
       let ys' = fv @ ys in
-      let typ = List.fold_right (fun x typ -> TFun((x,x.typ),typ)) ys TUnit in
+      let typ = List.fold_right (fun x typ -> TFun((x,x.typ),typ)) ys' TUnit in
       let f' = {f with typ=typ} in
       let f'' = List.fold_left (fun t x -> app2app t [Var x]) (Var f') fv in
       let defs1,t1' = lift2_aux ys' (subst f f'' t1) in
--- test/file.ml
+++ test/file.ml
@@ -1,15 +1,13 @@
-let open x = 1 in
-let close fp = 0 in
-let read fp = if fp = 0 then fail () else () in
-let rec read_n fp n =
-  if n <= 0
-  then close fp
-  else
-    (read fp;
-     read_n fp (n-1))
-in
-let n = ?n? in
-let fp = open () in
-let fp = read_n fp n in
-  if fp = 0 then () else fail ()
+(* camlp4/boot/camlp4boot.ml *)
+
+let open q x = if q=0 then x (stdin+1) 1 else fail() in
+let read ci q x = if ci=stdin || q=1 then x q else fail() in
+let close ci q x = if not(ci=stdin) && q=1 then x 0 else fail() in
+let fin q = if q=0 then () else fail() in
+
+let rec f2 ci q = if name = -1 then fin q else close ci q fin in
+let rec f1 n ci q = if n<=0 then f2 ci q else read ci q (f1 (n-1) ci) in
+let main n q = if name = -1 then f1 n stdin q else open q (f1 n) in
+  main n 0
+
 
