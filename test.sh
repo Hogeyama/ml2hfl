c1fc112cf55c7459fddf3f84a9cb585714cbac5a
uhiro <uhiro@382e5e6e-cd79-4ffa-aa24-b240b7b59e18>
2010-11-10 05:12:51 +0000

    Add examples
    git-svn-id: file:///home/ryosuke/repos_test/mochi@25 382e5e6e-cd79-4ffa-aa24-b240b7b59e18

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-TEST="intro1.ml intro2.ml intro3.ml sum.ml mult.ml max.ml mc91.ml repeat.ml lock.ml exception.ml neg1.ml neg2.ml neg3.ml neg4.ml fxx.ml fhnhn1.ml fhnhn2.ml intro1-e.ml intro2-e.ml intro3-e.ml sum-e.ml mult-e.ml mc91-e.ml lock-e.ml repeat-e.ml max-e.ml"
-TEST="intro1.ml intro2.ml intro3.ml sum.ml mult.ml max.ml mc91.ml repeat.ml fhnhn2.ml lock.ml neg4.ml sum-e.ml mult-e.ml max-e.ml mc91-e.ml repeat-e.ml lock-e.ml repeat2.ml"
+TEST="kmp.ml fact_exn.ml array_init.ml array_max.ml file.ml rev_append.ml rev_accum.ml rev_tricky.ml map_map.ml ack.ml recursive.ml intro1.ml intro2.ml intro3.ml sum.ml mult.ml max.ml mc91.ml repeat.ml fhnhn2.ml lock.ml neg4.ml sum-e.ml mult-e.ml max-e.ml mc91-e.ml repeat-e.ml lock-e.ml repeat2.ml"
+
+ulimit -t 10
 
 for i in $TEST
 do
