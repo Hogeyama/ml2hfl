d66f1f741446621c3d2bfeaaecc6e0bfc82e027b
uhiro <uhiro@382e5e6e-cd79-4ffa-aa24-b240b7b59e18>
2010-11-12 10:41:58 +0000

    Add test/bcopy_print.ml
    git-svn-id: file:///home/ryosuke/repos_test/mochi@38 382e5e6e-cd79-4ffa-aa24-b240b7b59e18

---
+++ test/bcopy_print.ml
@@ -0,0 +1,32 @@
+let make_array n i = assert (0 <= i && i < n); 0 in
+let update i n des x =
+  des i;
+  let a j = if i=j then x else des i in a
+in
+let print_int n = () in
+let f m src des =
+  let rec bcopy m src des i =
+    if i >= m
+    then des
+    else
+      let des = update i m des (src i) in
+        bcopy m src des (i+1)
+  in
+  let rec print_array m array i =
+    if i >= m
+    then ()
+    else
+      (print_int (des i);
+       print_array m array (i+1))
+  in
+    if c0 = 0
+    then
+      let array = bcopy n src des c0 in
+        print_array n array c0
+    else ()
+in
+let array1 = make_array n in
+let array2 = make_array n in
+  if n>0 && i=0
+  then f n array1 array2
+  else ()
