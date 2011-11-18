9b23db195409bdfff77500873fb6f3f14e1e9fa9
uhiro <uhiro@382e5e6e-cd79-4ffa-aa24-b240b7b59e18>
2011-11-18 08:55:09 +0000

    add examples
    git-svn-id: file:///home/ryosuke/repos_test/mochi@190 382e5e6e-cd79-4ffa-aa24-b240b7b59e18

---
+++ pmrs/list.ml
@@ -0,0 +1,39 @@
+
+(*
+let rec make_list m =
+  if m <= 0
+  then []
+  else m :: make_list (m-1)
+
+let rec make_list_list m =
+  if m <= 0
+  then []
+  else make_list (Random.int 0) :: make_list_list (m-1)
+*)
+let rec make_list (u:unit) =
+  if Random.int 0 > 0
+  then []
+  else 0 :: make_list ()
+
+let rec make_list_list (u:unit) =
+  if Random.int 0 > 0
+  then []
+  else make_list () :: make_list_list ()
+
+let head = function
+    [] -> assert false
+  | x::xs -> x
+
+let ne = function
+    [] -> false
+  | x::xs -> true
+
+let rec filter p = function
+    [] -> []
+  | x::xs -> if p x then x::(filter p xs) else filter p xs
+
+let rec map f = function
+    [] -> []
+  | x::xs -> f x :: map f xs
+
+let main m = map head (filter ne (make_list_list m))
