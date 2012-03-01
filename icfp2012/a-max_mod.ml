0f0598ed07e3c28d076845a88cec52855ef73abd
uhiro <uhiro@382e5e6e-cd79-4ffa-aa24-b240b7b59e18>
2012-03-01 03:32:43 +0000

    git-svn-id: file:///home/ryosuke/repos_test/mochi@317 382e5e6e-cd79-4ffa-aa24-b240b7b59e18

--- icfp2012/a-max_mod.ml
+++ icfp2012/a-max_mod.ml
@@ -1,13 +1,13 @@
 (* a-max_mod.ml: verification failed *)
 let make_array n i = n - i
-let rec array_max (n:int) (a:int->int) i m =
+let rec array_max i p (a:int->int) (n:int) m =
   if i >= n then
     m
   else
     let x = a i in
-    let z = if x>m then x else m in
-    array_max n a (i+1) z
-let main n i =
-  if n>0 && i>=0 && i<=0 then
-    let m = array_max n (make_array n) i (-1) in
+    let z = if x > m then x else m in
+    array_max (i+1) (ExtFuncs.f4 i p n m) a n z
+let main n =
+  if n > 0 then
+    let m = array_max 0 (ExtFuncs.f1 n) (make_array n) n (-1) in
     assert (m >= n)
--- icfp2012/extFuncs.mli
+++ icfp2012/extFuncs.mli
@@ -1,2 +1,8 @@
-val f : int -> int
-val g : int -> int
+val f1 : int -> int
+val g1 : int -> int
+val f2 : int -> int -> int
+val g2 : int -> int -> int
+val f3 : int -> int -> int -> int
+val g3 : int -> int -> int -> int
+val f4 : int -> int -> int -> int -> int
+val g4 : int -> int -> int -> int -> int
