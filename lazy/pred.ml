c5cc4a0ce0fdc2a1f09d3a6a3e5b45d6902f1ea0
uhiro <uhiro@382e5e6e-cd79-4ffa-aa24-b240b7b59e18>
2012-05-02 08:12:14 +0000

    git-svn-id: file:///home/ryosuke/repos_test/mochi@359 382e5e6e-cd79-4ffa-aa24-b240b7b59e18

---
+++ lazy/pred.ml
@@ -0,0 +1,23 @@
+(** Predicates *)
+
+type t = Var.t * Term.t list
+
+let pr ppf (pid, ts) =
+  Format.fprintf ppf "P[%a](%a)" Var.pr pid (Util.pr_list Term.pr ",") ts
+
+let make pid xs = pid, List.map Term.make_var xs
+
+let fvs (_, ts) = Util.concat_map Term.fvs ts
+let coeffs (_, ts) = Util.concat_map Term.coeffs ts
+
+let subst sub (pid, ts) =
+  (pid, List.map (Term.subst sub) ts)
+
+let subst_fixed sub (pid, ts) =
+  (pid, List.map (Term.subst_fixed sub) ts)
+
+let of_pid env pid =
+  pid, List.map Term.make_var (RefType.visible_vars env pid)
+
+let of_pid_vars env pid =
+  pid, RefType.visible_vars env pid
