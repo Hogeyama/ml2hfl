6e6b7bb4278f9453ce3cb44e8e18b7e6c62a08b7
uhiro <uhiro@382e5e6e-cd79-4ffa-aa24-b240b7b59e18>
2012-06-09 13:41:49 +0000

    git-svn-id: file:///home/ryosuke/repos_test/mochi@389 382e5e6e-cd79-4ffa-aa24-b240b7b59e18

--- relcomp/l-forall-leq.ml
+++ relcomp/l-forall-leq.ml
@@ -1,15 +1,16 @@
 (* safe but untypable *)
 let rec loop (u:unit) = loop u
 (*let hd (len, l) = l 0*)
-let tl (len, l) = (len - 1, fun i -> l (i + 1))
-let nil = (0, fun i -> loop ())
-let cons a (len, l) = (len + 1, fun i -> if i = 0 then a else l (i - 1))
+(*let tl (len, l) = (len - 1, fun i -> l (i + 1))*)
+(*let nil = (0, fun i -> loop ())*)
+(*let cons a (len, l) = (len + 1, fun i -> if i = 0 then a else l (i - 1))*)
 
 let rec make_list n =
   if n < 0 then
-    nil
+    (0, fun i -> loop ())(*nil*)
   else
-    cons n (make_list (n - 1))
+		  let (len, l) = make_list (n - 1) in
+    (len + 1, fun i -> if i = 0 then n else l (i - 1))(*cons n (len, l)*)
 
 let leq n x = x <= n
 
@@ -17,7 +18,7 @@ let rec for_all f (len, l) =
 		if len = 0 then
 	   true
   else if len >= 1 then
-	   f (l 0(*hd (len, l) fails*)) && for_all f (tl (len, l))
+	   f (l 0(*hd (len, l) fails*)) && for_all f (len - 1, fun i -> l (i + 1))(*(tl (len, l))*)
   else
     loop ()
 
