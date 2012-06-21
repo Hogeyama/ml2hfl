d0271b3b0aa1fb6460b65f27e99fb904f3580f48
uhiro <uhiro@382e5e6e-cd79-4ffa-aa24-b240b7b59e18>
2012-06-21 02:47:30 +0000

    git-svn-id: file:///home/ryosuke/repos_test/mochi@436 382e5e6e-cd79-4ffa-aa24-b240b7b59e18

--- relcomp/a-forall-leq.ml
+++
@@ -1,7 +0,0 @@
-let rec for_all f (sz, ar) =
-		if sz = 0 then
-	   true
-  else
-	   f (ar 0) && for_all f (sz - 1, fun i -> ar (i + 1))
-let main sz =
-  assert (for_all (fun x -> x <= sz) (sz, fun i -> sz - i))
