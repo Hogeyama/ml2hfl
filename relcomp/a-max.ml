7673543b835a86624a6b2a84bff87a2082ea414f
uhiro <uhiro@382e5e6e-cd79-4ffa-aa24-b240b7b59e18>
2012-06-22 04:13:27 +0000

    git-svn-id: file:///home/ryosuke/repos_test/mochi@447 382e5e6e-cd79-4ffa-aa24-b240b7b59e18

--- relcomp/a-max.ml
+++ relcomp/a-max.ml
@@ -1,12 +1,12 @@
-let rec loop u = loop u
-let rec array_max i (n:int) (ar:int->int) m =
+let rec array_max i n ar m =
   if i >= n then
     m
   else
     let x = ar i in
     let z = if x > m then x else m in
     array_max (i + 1) n ar z
+let rec rand_array x i = Random.int (x + 1)
 let main n x =
   if n > 0 && x >= 0 then
-    let m = array_max 0 n (fun i -> let n = Random.int 0 in if n <= x then n else loop ()) (-1) in
+    let m = array_max 0 n (rand_array x) (-1) in
     assert (m <= x)
