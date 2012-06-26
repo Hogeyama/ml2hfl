deddbde8daff109e5b57a3b4baa8be88f7499200
uhiro <uhiro@382e5e6e-cd79-4ffa-aa24-b240b7b59e18>
2012-06-26 16:09:27 +0000

    git-svn-id: file:///home/ryosuke/repos_test/mochi@471 382e5e6e-cd79-4ffa-aa24-b240b7b59e18

--- test_relcomp.sh
+++ test_relcomp.sh
@@ -3,7 +3,7 @@
 TEST="app-leq fhnhn-eq fhnhn-leq app-lin-ord2 app-lin-ord3 intro2 l-forall-leq app-succ intro3 repeat-add a-max a-checksum a-test-update l-length_cps-append"
 
 LIMIT=120
-OPTION="-enr2 -rsn 128 -rc"
+OPTION="-enr2 -rsn 128 -rc -spec relcomp/list_array.spec"
 
 for i in $TEST
 do
