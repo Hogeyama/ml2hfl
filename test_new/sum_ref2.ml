22a24bed025c360ab9d76361ab3b8fea1360b59c
Ryosuke SATO <ryosuke@kb.ecei.tohoku.ac.jp>
2012-12-12 18:27:41 +0900

    add test_new/sum_ref*

---
+++ test_new/sum_ref2.ml
@@ -0,0 +1,28 @@
+(*let sum n =
+  let s = ref 0 in
+  for i = 0 to n
+  do
+    s := !s + i
+  done;
+  !s
+*)
+
+let ref_ n (loc,store) = loc, (loc+1, fun i -> if loc = i then n else store i)
+let read i (_,store) = store i
+let update i n (loc,store) = (loc, fun j -> if i = j then n else store j)
+
+let rec loop () = loop ()
+let init_world = (0, fun _ -> loop ())
+let sum n world =
+  let s,world = ref_ 0 world in
+  let rec for_ i world =
+    if i > n
+    then world
+    else
+      let world = update s ((read s world) + i) world in
+        for_ (i+1) world
+  in
+  let world = for_ 0 world in
+    read s world
+
+let main n = assert (sum n init_world >= n)
