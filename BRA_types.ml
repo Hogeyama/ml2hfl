deadde3ad7f431a79861dce6dbe6a8285f5d0c00
xenophobia <xeno.lambda@i.softbank.jp>
2013-07-04 02:54:54 +0900

    fix prettyprinter of ranking function

--- BRA_types.ml
+++ BRA_types.ml
@@ -36,14 +36,16 @@ let updated_predicate_info pr new_coeffs new_error_paths =
 
 let pr_ranking_function fm { variables = vs; coefficients = coefficients} =
   let show_ranking_function {coeffs = cs; constant = const} =
+    let show_plus c = if c > 0 then "+" else "" in
     let fold_by acc v c =
       acc ^ (if c = 0 then ""
-	else if c = 1 then v.Id.name ^ "+"
-	else string_of_int c ^ v.Id.name ^ "+")
+	else if c = 1 then "+" ^ v.Id.name
+	else if c = (-1) then "-" ^ v.Id.name
+	else show_plus c ^ string_of_int c ^ v.Id.name)
     in
     let s = List.fold_left2 fold_by "" vs cs in
-    if const = 0 then String.sub s 0 (String.length s - 1)
-    else s ^ string_of_int const
+    if const = 0 then String.sub s 1 (String.length s - 1)
+    else s ^ show_plus const ^ string_of_int const
   in
   match coefficients with
     | [] -> Format.fprintf fm "0"
