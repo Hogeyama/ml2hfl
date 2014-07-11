cd7da8709bd463057b9d51973b992264e8ad54d7
Ryosuke SATO <ryosuke@kb.is.s.u-tokyo.ac.jp>
2014-07-11 18:21:40 +0900

    minor

--- test_relabs/append_nil2_nth.ml
+++ test_relabs/append_nil2_nth.ml
@@ -1,18 +1,12 @@
-let rec append (xs, ys) =
-  match xs with
-    [] -> ys
-  | x::xs' -> x :: append (xs', ys)
-
 let rec make_list n =
   if n < 0
   then []
   else Random.int 0 :: make_list (n-1)
 
-let rec list_eq xsys =
-  match xsys with
-    [], [] -> true
-  | x::xs', y::ys' -> x = y && list_eq (xs', ys')
-  | _ -> false
+let rec append (xs, ys) =
+  match xs with
+    [] -> ys
+  | x::xs' -> x :: append (xs', ys)
 
 let main i n =
   let xs = make_list n in
