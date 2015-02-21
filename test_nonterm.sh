20194082e5478ecb9d66d442e727a23264b51bfb
kuwahara <kuwahara111011@gmail.com>
2015-02-21 14:40:14 +0900

    Fix test_nonterm.sh

--- test_nonterm.sh
+++ test_nonterm.sh
@@ -1,5 +1,5 @@
 # Non-terminating
-ulimit -t20 | ./mochi.opt -non-termination -fpat "-eahccs 3" test_infer/fib_nonterm.ml
+ulimit -t20 | ./mochi.opt -non-termination -fpat "-eahccs 3" test_infer/fib_nonterm2.ml
 ulimit -t20 | ./mochi.opt -non-termination -fpat "-eahccs 3" test_infer/loop.ml
 # Unknown
 ulimit -t20 | ./mochi.opt -non-termination -fpat "-eahccs 3" test_infer/terminate.ml
