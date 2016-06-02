e6e4e024af41bd5e34d0eb28a3def4c9dc0aba28
Keiichi Watanabe <udon.watanabe@gmail.com>
2016-06-02 15:54:41 +0900

    testcases for fair-nonterm

---
+++ test_fair_non_termination/new/todo/passing_cond-buggy.ml
@@ -0,0 +1,22 @@
+(*
+  HorSatPのNot Implementedにぶつかる
+*)
+
+(* CAV 2015 *)
+
+let isPositive (n:int) = n > 0
+let isNegative (n:int) = event "A"; n < 0
+let rec f (cond:int->bool) (x:int) =
+    (*  if cond x then f cond (x-1) else () *)
+    if cond x then f cond x else ()
+let main () =
+  let r = read_int () in
+  let n = read_int () in
+  if r > 0 then
+    f isPositive n
+  else
+    f isNegative n
+
+(*{SPEC}
+  fairness: (A, Never)
+{SPEC}*)
